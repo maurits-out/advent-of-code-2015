1d1041a5bc375f63f6b47343f02766bee6366d78
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2019-11-24 20:44:06 +0100

    Day 8 second part.

--- src/main/java/day08/Matchsticks.java
+++ src/main/java/day08/Matchsticks.java
@@ -1,15 +1,22 @@
 package day08;
 
+import java.util.function.ToIntFunction;
 import java.util.stream.Stream;
 
 import static support.InputLoader.loadInput;
 
 public class Matchsticks {
 
+    private int mapAndSum(Stream<String> lines, ToIntFunction<String> function) {
+        return lines.mapToInt(function).sum();
+    }
+
     int part1(Stream<String> lines) {
-        return lines
-                .mapToInt(line -> line.length() - countCharactersInMemory(line))
-                .sum();
+        return mapAndSum(lines, line -> line.length() - countCharactersInMemory(line));
+    }
+
+    int part2(Stream<String> lines) {
+        return mapAndSum(lines, line -> countCharactersEncoded(line) - line.length());
     }
 
     private int countCharactersInMemory(String line) {
@@ -28,9 +35,21 @@ public class Matchsticks {
         return count;
     }
 
+    private int countCharactersEncoded(String line) {
+        return 2 + line.chars()
+                .map(ch -> {
+                    if (ch == '"' || ch == '\\') {
+                        return 2;
+                    }
+                    return 1;
+                })
+                .sum();
+    }
+
     public static void main(String[] args) {
         String input = loadInput("day08-input.txt");
         Matchsticks matchsticks = new Matchsticks();
         System.out.printf("Part 1: %d\n", matchsticks.part1(input.lines()));
+        System.out.printf("Part 2: %d\n", matchsticks.part2(input.lines()));
     }
 }
--- src/test/java/day08/MatchsticksTest.java
+++ src/test/java/day08/MatchsticksTest.java
@@ -2,17 +2,26 @@ package day08;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatchsticksTest {
 
     private final Matchsticks matchsticks = new Matchsticks();
 
+    private static final List<String> LINES = asList("\"\"", "\"abc\"", "\"aaa\\\"aaa\"", "\"\\x27\"");
+
     @Test
     void part1() {
-        Stream<String> lines = Stream.of("\"\"", "\"abc\"", "\"aaa\\\"aaa\"", "\"\\x27\"");
-        assertEquals(12, matchsticks.part1(lines));
+        assertEquals(12, matchsticks.part1(LINES.stream()));
+    }
+
+    @Test
+    void part2() {
+        assertEquals(19, matchsticks.part2(LINES.stream()));
     }
 }
